14c88d523b4240ee2d2c3ca34ed34a72371c3d99
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-19 22:15:56 +0800

    sebuah class dengan 8 (delapan) atribut, 4 (empat) method (selain getter, setter, dan CRUD)

---
+++ src/Restaurant/Menu.java
@@ -0,0 +1,35 @@
+//Renata Febriana Jacom
+//19630453
+//4c Reguler Pagi BanjarBaru
+
+
+package Restaurant;
+
+public class Menu {
+    //8 atribut terlebih dahulu
+    public String namapelanggan;
+    public String namamakanan;
+    public String namaminuman;
+    public String jumlahmakanan;
+    public String jumlahminuman;
+    public int hargamakanan;
+    public int hargaminuman;
+    public int kembalian;
+
+    //4 method
+   public  void tampilkannamapelanggan(){
+        System.out.println("Selamat Datang" +namapelanggan);
+    }
+    public void tampilkanpesanan(){
+       System.out.println("Makanan yang anda pesan : " +namamakanan);
+       System.out.println("Minuman yang anda pesan : " +namaminuman);
+    }
+    public void jumlahpesanan(){
+       System.out.println("Jumlah makanan yang di pesan : " +jumlahmakanan);
+       System.out.println("Jumlah minuman yang dipesan  : " +jumlahminuman);
+    }
+    public void hargamenu(){
+       System.out.println(+namamakanan "=" +hargamakanan);
+       System.out.println(+namaminuman "=" +hargaminuman);
+    }
+}
