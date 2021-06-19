9c521645660178c847750fed394bd610a909b4e8
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-19 23:42:59 +0800

    object(yang benar)

--- src/subclass/struk.java
+++ src/subclass/struk.java
@@ -2,18 +2,18 @@ package subclass;
 
 import Restaurant.Menu;
 
-public class MenuSubclass  extends Menu {
+    public class struk extends Menu {
 
         //menambahkan 3 atribut
         public String namakasir;
-        public int totalbayar;
-        public int kembalian;
+        public String totalbayar;
+        public String kembalian;
 
         //menambahkan 2 method;
         public void kasir(){
             System.out.println("Nama Kasir : "+namakasir);
         }
-        public void struk(){
+        public void hasil(){
             System.out.println("Total yang harus bayar : " +totalbayar);
             System.out.println("Kembalian anda : " +kembalian);
         }
